3b1965458da5cb6428e79d67b721055078768f40
TaoOrz <kairos.ou@gdautotoll.com.cn>
2025-08-18 17:06:58 +0800

    updata

---
+++ CICDTest/CICDTest/CICDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTestApp.swift
+//  CICDTest
+//
+//  Created by mac on 2025/8/18.
+//
+
+import SwiftUI
+
+@main
+struct CICDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
